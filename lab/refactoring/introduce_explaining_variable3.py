f67d2b954a14e3df48393fc7a13200de922b76e0
Christopher Barnes <chris.barnes.2000@me.com>
2021-02-04 11:48:04 -0800

    [Complete]: Activity 4: Introduce_Explaining_Variable3

--- lab/refactoring/introduce_explaining_variable3.py
+++ lab/refactoring/introduce_explaining_variable3.py
@@ -4,9 +4,9 @@ WELL_DONE = 900000
 MEDIUM = 600000
 COOKED_CONSTANT = 0.05
 
+
 def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    if desired_state == 'well-done' and time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE: 
-        return True
-    if desired_state == 'medium' and time * temperature * pressure * COOKED_CONSTANT >= MEDIUM:
-        return True
-    return False
+    _current_state = time * temperature * pressure * COOKED_CONSTANT
+    is_well_done = _current_state >= WELL_DONE
+    is_medium = _current_state >= MEDIUM
+    return (is_well_done and desired_state == 'well-done') or (is_medium and desired_state == 'medium')
