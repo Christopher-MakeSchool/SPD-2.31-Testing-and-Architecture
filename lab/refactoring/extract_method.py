974134ea85f47925b696663b3288112f520e3716
Christopher Barnes <chris.barnes.2000@me.com>
2021-02-04 10:39:37 -0800

    [Complete]: Activity 1: Extract Method

--- lab/refactoring/extract_method.py
+++ lab/refactoring/extract_method.py
@@ -1,28 +1,39 @@
 # Written by Kamran Bigdely
 # Example for Compose Methods: Extract Method.
-import math 
+import math
 
-def print_stat():
-    grade_list = []
-    # Get the inputs from the user
-    n_student = 5
-    for _ in range(0, n_student):
-        grade_list.append(int(input('Enter a number: ')))
-
-    # Calculate the mean and standard deviation of the grades
-    sum = 0 # Do you think 'sum' is a good var name? Run pylint to figure out!
-    for grade in grade_list:
-        sum = sum + grade
-    mean = sum / len(grade_list)
-    sd = 0 # standard deviation
-    sum_of_sqrs = 0
-    for grade in grade_list:
-        sum_of_sqrs += (grade - mean) ** 2
-    sd = math.sqrt(sum_of_sqrs / len(grade_list))
+def print_stat(mean, sd):
     # print out the mean and standard deviation in a nice format.
     print('****** Grade Statistics ******')
     print("The grades's mean is:", mean)
     print('The population standard deviation of grades is: ', round(sd, 3))
     print('****** END ******')
 
-print_stat()
+
+def get_grades():
+    grade_list = []
+    # Get the inputs from the user
+    n_student = 5
+    for _ in range(0, n_student):
+        grade_list.append(int(input('Enter a number: ')))
+    return grade_list
+
+
+def calculate_grade_stats(grade_list):
+    # Calculate the mean and standard deviation of the grades
+    total = sum(grade_list)
+    mean = total/len(grade_list)
+    sum_of_sqrs = 0
+    sd = 0  # standard deviation
+
+    for grade in grade_list:
+      sum_of_sqrs += (grade - mean) ** 2
+
+    sd = math.sqrt(sum_of_sqrs / len(grade_list))
+    return mean, sd
+
+
+if __name__ == "__main__":
+    grade_list = get_grades()
+    mean, sd = calculate_grade_stats(grade_list)
+    print_stat(mean, sd)
