12cc591ba6dda36ac99890b1d6f0e077812bd7c0
Christopher Barnes <chris.barnes.2000@me.com>
2021-02-22 14:59:46 -0800

    [Complete]: ex) 23: Extract Class2

--- lab/refactoring/Refactoring_Other_Refactoring/extract_class2.py
+++ lab/refactoring/Refactoring_Other_Refactoring/extract_class2.py
@@ -1,20 +1,37 @@
 # by Kami Bigdely
 # Extract class
-first_names = ['elizabeth', 'Jim']
-last_names = ['debicki', 'Carrey']
-birth_year = [1990, 1962]
-movies = [['Tenet', 'Vita & Virgina', 'Guardians of the Galexy', 'The Great Gatsby'],\
-     ['Ace Ventura', 'The Mask', 'Dubm and Dumber', 'The Truman Show', 'Yes Man']]
-emails = ['deb@makeschool.com', 'jim@makeschool.com']
+
+class Person():
+    def __init__(self, first_name, last_name, birth_year, email=None, movies=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year
+        self.email = email
+        self.movies = movies
+
+    def add_email(self, email_address):
+        self.email = email_address
+
+    def moves_played(self, movies):
+        self.moveis.append(movies)
+
 
 def send_hiring_email(email):
     print("email sent to: ", email)
-    
-for i, value in enumerate(emails):
-    if birth_year[i] > 1985:
-        print(first_names[i], last_names[i])
-        print('Movies Played: ', end='')
-        for m in movies[i]:
-            print(m, end=', ')
-        print()
-        send_hiring_email(value)
+
+
+if __name__ == "__main__":
+    elizabeth_debicki = Person('Elizabeth', 'debicki', 1990, 'deb@makeschool.com', 
+                               ['Tenet', 'Vita & Virgina', 'Guardians of the Galexy', 'The Great Gatsby'])
+    jim_carrey = Person('Jim', 'Carrey', 1962, 'jim@makeschool.com',
+                        ['Ace Ventura', 'The Mask', 'Dubm and Dumber', 'The Truman Show', 'Yes Man'])
+    people = [elizabeth_debicki, jim_carrey]
+
+    for person in people:
+        if person.birth_year > 1985:
+            print(person.first_name, person.last_name)
+            print('Movies Played: ', end='')
+            for m in person.movies:
+                print(m, end=', ')
+            print()
+            send_hiring_email(person.email)
--- lab/refactoring/readme.md
+++ lab/refactoring/readme.md
@@ -100,7 +100,7 @@ In this series of exercises, we practice various refactoring techniques. You may
 
 ### Exercise 23: `Extract Class` Technique
 
-- [ ] Study [**extract_class2.py**](Refactoring_Other_Refactoring/extract_class2.py). Use `extract class` technique in combination with any other refactoring technique you've learned to improve readability/modularity of the code.
+- [X] Study [**extract_class2.py**](Refactoring_Other_Refactoring/extract_class2.py). Use `extract class` technique in combination with any other refactoring technique you've learned to improve readability/modularity of the code.
 
 ### Exercise 24: `Extract Class` Technique
 
